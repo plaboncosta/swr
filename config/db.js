9e2c7b708234aecbf5c150c281e69d7d22b4adff
plaboncosta <plabonjoseph@gmail.com>
2020-05-12 23:32:56 +0600

    add space

--- config/db.js
+++ config/db.js
@@ -4,4 +4,4 @@ const uri = process.env.productionMongoURI ? require('./default').productionMong
 // Connect to MongoDB
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true})
 .then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
+.catch(err => console.log(err)); 
