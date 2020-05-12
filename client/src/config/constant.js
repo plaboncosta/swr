d812290898b7da79cfff98654da03613389f5377
plaboncosta <plabonjoseph@gmail.com>
2020-05-12 23:29:43 +0600

    file change

--- client/src/config/constant.js
+++ client/src/config/constant.js
@@ -1,4 +1,4 @@
-const production = true;
+const production = false;
 const domain = production ? 'http://45.132.242.146/' : 'http://localhost:5000/';
 
 export const backendApiUrl = `${domain}`;
--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = require('./default').mongoURI;
+const uri = process.env.productionMongoURI ? require('./default').productionMongoURI : require('./default').developmentMongoURI;
 
 // Connect to MongoDB
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true})
